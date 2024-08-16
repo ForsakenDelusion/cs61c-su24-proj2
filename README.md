ffbd81794bbc090cb160ad4ab9f7c01a3085a4f6
Forsaken Delusion <2430385640cy@gmail.com>
2024-08-16 15:27:44 +0800

    finish task2

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 61C Project 2: CS61Classify
+
+## Task1
+
+绝对值函数
+
+错误很好改，以至于我再改第一个错误的时候直接把内存错误改了，输-mc半天没反应我还以为我的版本有问题。
+
+## Task2
+
+该函数是将所有的负数都变成0
+
+>The ReLU function takes in an integer array and sets every negative value in the array to 0. Positive values in the array are unchanged. In other words, for each element x in the array, ReLU computes max(x, 0).
+
--- src/relu.s
+++ src/relu.s
@@ -14,20 +14,26 @@
 # ==============================================================================
 relu:
     # Prologue
-
-
+    ebreak
+    addi t0 x0 1
+    blt a1 t0 error_exit
+    mv t1 a1 #t1 is the length of array
+    addi t2 x0 0 #t2 is the counter
+    addi t3 x0 0 #t3 is the offset
 loop_start:
-
-
-
-
-
-
-
-
+    addi t4 x0 0 #t4 is the value of array[i]
+    add t5 x0 a0 #t5 is the bAddr
+    slli t3 t2 2
+    add t5 t5 t3 #get the bAddr + offset
+    lw t4  0(t5)
+    blt t4 zero loop_continue
+    addi t2 t2 1
+    blt t2 a1 loop_start
+    j loop_end
+    
 loop_continue:
-
-
+    sw x0 0(t5)
+    blt t2 a1 loop_start
 
 loop_end:
 
@@ -36,3 +42,7 @@ loop_end:
 
 
     jr ra
+    
+error_exit:
+    li a0 36
+    j exit
